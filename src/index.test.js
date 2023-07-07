6a9af242322c3662d0ca378c9d1ac6f7ceae31cf
jared <114547220+goldjared@users.noreply.github.com>
2023-07-07 17:20:21 -0400

    rewrite tests, check for hit val, and isSunk

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,17 @@
 import { ship } from "./index.js";
+const testShip = ship(2);
 
 test("should return object with ship length, hits, and whether sunk/or not", () => {
-  expect(ship()).toEqual({
-    length: 0,
+  expect(testShip.getShip()).toEqual({
+    length: 2,
     hits: 0,
     sunken: false
   });
 });
+ test('hit will hit for value of 1', () => {
+  expect(testShip.hit()).toBe(1);
+})
+
+test('isSunk is false after no ship hit on length 2 ship', () => {
+  expect(testShip.isSunk()).toBe(false)
+})
