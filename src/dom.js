3e7b2520dbf0481513bcf4dff033af566b818f63
jared <114547220+goldjared@users.noreply.github.com>
2023-07-15 21:49:18 -0400

    append container to dom body, add class and dataset to each board spot element

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,15 @@
 function gameBoardDisplay(board) {
-  const container = document.querySelector('.container')
+  const container = document.createElement('div')
+  container.classList.add('container')
+  const body = document.querySelector('body')
   board.getBoard().forEach(spot => {
     const boardSpot = document.createElement('div')
+    boardSpot.classList.add('game-square')
+    boardSpot.dataset.coord = spot.data
     boardSpot.textContent = spot.data;
     container.append(boardSpot);
   })
+  body.append(container);
 }
 
 export { gameBoardDisplay }
